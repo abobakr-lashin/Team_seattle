45b05b2a24cbec5d1a751d88bc17d0e7d20e201e
abobakr-lashin <abobakrlashin1@gmail.com>
2024-09-17 02:09:10 +0300

    s

--- src/Components/appbar/Header.jsx
+++ src/Components/appbar/Header.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
 import video from "./BannerFinish.mp4";
+import video2 from "./video2.mp4";
 import NavPar from "./NavPar";
 import "./Header.css";
 import { Box, Snackbar, Alert, SpeedDial, SpeedDialIcon, SpeedDialAction } from "@mui/material";
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import PhoneIcon from '@mui/icons-material/Phone';
 import FormContainer from "./FormContainer";
+import React, { useEffect, useState } from 'react';
 
 // Post Data To fireBase (Ensure you have the logic for posting data)
 
@@ -18,7 +19,27 @@ export default function Header() {
         { icon: <WhatsAppIcon />, name: 'WhatsApp', link: "https://api.whatsapp.com/send/?phone=971502135701&text&type=phone_number&app_absent=0" },
         { icon: <PhoneIcon />, name: 'Phone', link: "tel:+971502135701" },
     ];
+        const [videoSrc, setVideoSrc] = useState("");
+      
+        useEffect(() => {
+          const handleResize = () => {
+            // إذا كان عرض الشاشة أكبر من 1024px، قم بتعيين فيديو للشاشات الكبيرة
+            if (window.innerWidth > 1024) {
+              setVideoSrc(video);
+            } else {
+              // إذا كان عرض الشاشة أقل من 1024px، قم بتعيين فيديو للشاشات الصغيرة
+              setVideoSrc(video2);
+            }
+          }; handleResize();
 
+          // أضف مستمع للحدث للتعامل مع تغيير حجم الشاشة
+          window.addEventListener('resize', handleResize);
+      
+          // قم بإزالة المستمع عند إلغاء تحميل المكون
+          return () => {
+            window.removeEventListener('resize', handleResize);
+          };
+        }, []);
     const handleActionClick = (link, name) => {
         if (name === 'Phone') {
             window.location.href = link; // Initiates the phone call
@@ -36,9 +57,9 @@ export default function Header() {
     return (
         <>
             <div className="header-container">
-                <video className="video-background" autoPlay loop muted>
-                    <source src={video} type="video/mp4" />
-                </video>
+            <video className="video-background" autoPlay loop muted>
+      <source src={videoSrc} type="video/mp4" />
+    </video>
                 <NavPar />
 
 <div className="Formheader">
