e98096c2ea056eedc4496039a7daf6aaf5a752cb
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2024-09-29 15:58:50 +0300

    first commit

--- src/admin/pageDb/AddCardBuy.jsx
+++ src/admin/pageDb/AddCardBuy.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import { Box, CircularProgress } from '@mui/material';
 
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 export default function AddCardBuy() {
     const Navigate = useNavigate()
     const [FileURLs, setFileURLs] = useState([])
@@ -421,8 +422,17 @@ export default function AddCardBuy() {
                         </select>
                     </div>
                     <div className="form-group">
-                        <textarea value={formData.text} style={{ margin: '20px', width: '80%', height: '300px' }} onChange={handleInputChange} placeholder='Enter your Description' name="text" id="text">
-                        </textarea>
+                    <CKEditor
+                            editor={ClassicEditor}
+                            data={formData.text || ""}
+                            onChange={(event, editor) => {
+                                const data = editor.getData();
+                                setFormData({ ...formData, text: data });
+                            }}
+                            config={{
+                                height: '400px', 
+                            }}
+                        />
                     </div>
                     <br /><br />
                     <button type="submit" disabled={loading}>
--- src/admin/pageDb/UpdateBuy.jsx
+++ src/admin/pageDb/UpdateBuy.jsx
@@ -7,6 +7,8 @@ import { firestore, storage } from '../../firebaseConfig';
 import { toast } from 'react-toastify';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Box, CircularProgress } from '@mui/material';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export default function UpdateBuy() {
     const { id } = useParams();
@@ -428,9 +430,17 @@ export default function UpdateBuy() {
                         </select>
                     </div>
                     <div className="form-group">
-                        <textarea value={formData.text} style={{ margin: '20px', width: '80%', height: '300px' }} onChange={handleInputChange} placeholder='Enter your Description' name="text" id="text">
-
-                        </textarea>
+                        <CKEditor
+                            editor={ClassicEditor}
+                            data={formData.text || ""}
+                            onChange={(event, editor) => {
+                                const data = editor.getData();
+                                setFormData({ ...formData, text: data });
+                            }}
+                            config={{
+                                height: '400px',
+                            }}
+                        />
                     </div>
                     <button type="submit" disabled={loading}>
                         {loading ? 'Updating...' : 'Update'}
