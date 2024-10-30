d46238b57721560e551f9b3425d786c3e7737f5d
abobakr-lashin <abobakrlashin1@gmail.com>
2024-10-30 11:58:40 +0300

    o

--- src/Components/discover/Discover.jsx
+++ src/Components/discover/Discover.jsx
@@ -23,22 +23,22 @@ export default function Discover() {
       {`  `}exceptional professional services across real estate,
     <br class="small-screen-hide" /> travel, contracting, mortgage, and digital marketing.
     <br class="small-screen-hide" />
-    We are committed to delivering tailored solutions that {`  `}
+    {`  `}We are committed to delivering tailored solutions that {`  `}
     <br class="small-screen-hide" />
-    meet the unique needs of our clients.
+    {`  `} meet the unique needs of our clients.
     <br />
     <br/>
-    Our expert team in real estate helps you find ideal
+    {`  `} Our expert team in real estate helps you find ideal
     <br class="small-screen-hide" /> residential, commercial, or investment properties.
     <br class="small-screen-hide" />
-    Our travel services offer seamless and memorable journeys,
+    {`  `} Our travel services offer seamless and memorable journeys,
     <br class="small-screen-hide" /> while our mortgage team provides comprehensive financing guidance.
     <br class="small-screen-hide" />
-    Our digital marketing agency boosts your brand's online presence
+    {`  `}Our digital marketing agency boosts your brand's online presence
     <br class="small-screen-hide" /> with cutting-edge strategies.
     <br />
     <br  />
-    Professionalism, integrity, and customer satisfaction are our core values.
+    {`  `}Professionalism, integrity, and customer satisfaction are our core values.
     <br class="small-screen-hide" />
 </p>
         </div>
